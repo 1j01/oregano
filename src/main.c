36da9add7aa4d6b84322eead7bac15b96bb879ce
Isaiah Odhner <isaiahodhner@gmail.com>
2022-09-19 22:48:33 -0400

    Make smiley particles

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include "wasm4.h"
+#include <math.h>
+
+#define MAX_PARTICLES 500
 
 const uint8_t smiley[] = {
     0b11000011,
@@ -11,15 +14,58 @@ const uint8_t smiley[] = {
     0b11000011,
 };
 
-void update () {
+struct Particle
+{
+    double x;
+    double y;
+    double vx;
+    double vy;
+    int life;
+};
+
+struct Particle particles[MAX_PARTICLES];
+
+void add_particle(struct Particle particle)
+{
+    for (int i = 0; i < MAX_PARTICLES; i++)
+    {
+        if (particles[i].life <= 0)
+        {
+            particles[i] = particle;
+            break;
+        }
+    }
+}
+
+int t = 0;
+
+void update()
+{
+    t++;
+
     *DRAW_COLORS = 2;
     text("Hello from C!", 10, 10);
 
     uint8_t gamepad = *GAMEPAD1;
-    if (gamepad & BUTTON_1) {
+    if (gamepad & BUTTON_1)
+    {
         *DRAW_COLORS = 4;
+        
+        struct Particle p = {80, 80, sin(t), cos(t), 100};
+        add_particle(p);
     }
 
-    blit(smiley, 76, 76, 8, 8, BLIT_1BPP);
-    text("Press X to blink", 16, 90);
+    for (int i = 0; i < MAX_PARTICLES; i++)
+    {
+        struct Particle* p = &particles[i];
+        if (p->life > 0)
+        {
+            p->x += p->vx;
+            p->y += p->vy;
+            p->life -= 1;
+            blit(smiley, (int32_t)p->x, (int32_t)p->y, 8, 8, BLIT_1BPP);
+        }
+    }
+    text("Press X to spawn", 16, 90);
+    text("some particles!", 22, 103);
 }

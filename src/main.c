72a414e6069299b904b475bfd451181939bf80a7
Isaiah Odhner <isaiahodhner@gmail.com>
2022-09-19 23:24:51 -0400

    WIP: add sticks between particles

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <math.h>
 
 #define MAX_PARTICLES 500
+#define MAX_STICKS 300
+#define ITERATIONS 4
 
 const uint8_t smiley[] = {
     0b11000011,
@@ -23,18 +25,44 @@ struct Particle
     int life;
 };
 
-struct Particle particles[MAX_PARTICLES];
+struct Stick
+{
+    struct Particle *a;
+    struct Particle *b;
+    double restLength;
+    int life;
+};
 
-void add_particle(struct Particle particle)
+struct Particle particles[MAX_PARTICLES];
+struct Stick sticks[MAX_STICKS];
+
+// stupid methods just for adding to arrays
+// I'm learning C.
+// Probably should track the count, and shift elements when removing.
+int add_particle(struct Particle particle)
 {
     for (int i = 0; i < MAX_PARTICLES; i++)
     {
         if (particles[i].life <= 0)
         {
             particles[i] = particle;
-            break;
+            return i;
         }
     }
+    return -1;
+}
+
+int add_stick(struct Stick stick)
+{
+    for (int i = 0; i < MAX_STICKS; i++)
+    {
+        if (sticks[i].life <= 0)
+        {
+            sticks[i] = stick;
+            return i;
+        }
+    }
+    return -1;
 }
 
 int t = 0;
@@ -50,14 +78,48 @@ void update()
     if (gamepad & BUTTON_1)
     {
         *DRAW_COLORS = 4;
-        
+
         struct Particle p = {80, 80, sin(t), cos(t), 100};
-        add_particle(p);
+        int p_i = add_particle(p);
+        struct Stick stick = {&particles[p_i], &particles[p_i - 1], 10, 100};
+        add_stick(stick);
+    }
+
+    // for (int it = 0; it < ITERATIONS; it++)
+    // {
+    //     for (int i = 0; i < MAX_STICKS; i++)
+    //     {
+    //         struct Stick *s = &sticks[i];
+    //         if (s->life > 0)
+    //         {
+    //             double dx = s->a->x - s->b->x;
+    //             double dy = s->a->y - s->b->y;
+    //             double d = sqrt(dx * dx + dy * dy);
+    //             double delta = s->restLength - d;
+    //             double force = delta * 0.01;
+    //             s->a->vx -= force * dx;
+    //             s->a->vy -= force * dy;
+    //             s->b->vx += force * dx;
+    //             s->b->vy += force * dy;
+    //         }
+    //     }
+    // }
+
+    for (int i = 0; i < MAX_STICKS; i++)
+    {
+        struct Stick *s = &sticks[i];
+        // if (abs(s->a->x - s->b->x) > 160) s->life = 0;
+        // if (abs(s->a->y - s->b->y) > 160) s->life = 0;
+        if (s->life > 0)
+        {
+            // s->life -= 1;
+            line((int32_t)s->a->x, (int32_t)s->a->y, (int32_t)s->b->x, (int32_t)s->b->y);
+        }
     }
 
     for (int i = 0; i < MAX_PARTICLES; i++)
     {
-        struct Particle* p = &particles[i];
+        struct Particle *p = &particles[i];
         if (p->life > 0)
         {
             p->x += p->vx;
